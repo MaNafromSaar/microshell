b9b0c33bb34f002a512aaeeea73d3c4fc905f24c
MaNafromSaar <127291363+MaNafromSaar@users.noreply.github.com>
2025-04-02 18:54:14 +0200

    Update mic21.c

--- mic21.c
+++ mic21.c
@@ -3,30 +3,26 @@
 #include <sys/wait.h>
 #include <unistd.h>
 
-void prerr(char *str)
-{
+void prerr(char *str) {
 	while(*str)
 		write(2, str++, 1);
 }
 
-void cd(int argc, char **argv)
-{
+void cd(int argc, char **argv) {
 	if(argc != 2)
 		return(prerr("error: cd: bad arguments\n"));
 	if(chdir(argv[1]) < 0)
 		prerr("error: cd: cannot change to "), prerr(argv[1]), prerr("\n"), exit(1);
 }
 
-void runcmd(int argc, char **argv, char **envp, int has_pipe)
-{
+void runcmd(int argc, char **argv, char **envp, int has_pipe) {
 	int fd[2];
 	int pid;
 	if(!strcmp(argv[0], "cd"))
 		return(cd(argc, argv));
 	if((has_pipe && pipe(fd) == -1) || (pid = fork()) == -1)
 		prerr("error: fatal\n"), exit(1);
-	if(!pid)
-	{
+	if(!pid) {
 		if(has_pipe && (dup2(fd[1], 1) == -1 || close(fd[0]) == -1 || close(fd[1]) == -1))
 			prerr("error: fatal\n"), exit(1);
 		execve(argv[0], argv, envp);
@@ -37,10 +33,8 @@ void runcmd(int argc, char **argv, char **envp, int has_pipe)
 		prerr("error: fatal\n"), exit(1);	
 }
 
-int main(int argc, char **argv, char **envp)
-{
-	for(int i = 1; i < argc; i++)
-	{
+int main(int argc, char **argv, char **envp) {
+	for(int i = 1; i < argc; i++) {
 		int cmd = i, has_pipe = 0;
 		while((argv[i] && strcmp(argv[i], ";")) && strcmp(argv[i], "|"))
 			i++;
